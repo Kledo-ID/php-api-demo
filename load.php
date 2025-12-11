c3b3f635c0acadd47887027579afd201917f59bc
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2025-12-11 17:35:51 +0700

    feat: support personal access token

--- load.php
+++ load.php
@@ -2,13 +2,5 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use Josantonius\Session\Facades\Session;
-
-Session::start();
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-$clientId = $_ENV['CLIENT_ID'];
-$clientSecret = $_ENV['CLIENT_SECRET'];
-$redirectUri = $_ENV['REDIRECT_URI'];
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'load.php';
-
-Josantonius\Session\Facades\Session::destroy();
-
-header('Location: ./index.php');
-
-exit();
