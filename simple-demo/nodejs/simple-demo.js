01467f15748a6ae71746c1b891c01d27b04ec4d3
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2026-02-03 12:54:57 +0700

    fix: simple demo nodejs

--- simple-demo/nodejs/simple-demo.js
+++ simple-demo/nodejs/simple-demo.js
@@ -1,14 +1,36 @@
 /**
- * Kledo API Demo - Node.js (tanpa npm, Node 18+)
+ * Kledo API Demo - Node.js (tanpa npm)
  * Jalankan: node simple-demo.js
  */
+
+const https = require('https');
+const http = require('http');
 
 const API_HOST = 'http://xxx.api.kledo.com/api/v1';
 const ACCESS_TOKEN = 'your_token_here';
 
-const url = `${API_HOST}/finance/accounts?sort_by=name&order_by=asc&per_page=5`;
-const res = await fetch(url, {
-  headers: { 'Accept': 'application/json', 'Authorization': `Bearer ${ACCESS_TOKEN}` }
-});
-const data = await res.json();
-console.log(JSON.stringify(data, null, 2));
+// Untuk local/dev dengan self-signed cert: true (hapus di production!)
+const INSECURE_SSL = true;
+
+(async function () {
+  const url = new URL(`${API_HOST}/finance/accounts?sort_by=name&order_by=asc&per_page=5`);
+  const lib = url.protocol === 'https:' ? https : http;
+
+  const data = await new Promise((resolve, reject) => {
+    const opts = {
+      headers: { 'Accept': 'application/json', 'Authorization': `Bearer ${ACCESS_TOKEN}` }
+    };
+    if (url.protocol === 'https:' && INSECURE_SSL) {
+      opts.rejectUnauthorized = false;
+    }
+    const req = lib.request(url, opts, res => {
+      let body = '';
+      res.on('data', chunk => body += chunk);
+      res.on('end', () => resolve(JSON.parse(body)));
+    });
+    req.on('error', reject);
+    req.end();
+  });
+
+  console.log(JSON.stringify(data, null, 2));
+})();
