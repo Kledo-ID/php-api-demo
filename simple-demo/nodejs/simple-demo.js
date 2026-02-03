db60c3700c80b26eb38d374c88ab702ac9b11b1d
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2026-02-03 12:36:31 +0700

    feat: add node js example

---
+++ simple-demo/nodejs/simple-demo.js
@@ -0,0 +1,14 @@
+/**
+ * Kledo API Demo - Node.js (tanpa npm, Node 18+)
+ * Jalankan: node simple-demo.js
+ */
+
+const API_HOST = 'http://xxx.api.kledo.com/api/v1';
+const ACCESS_TOKEN = 'your_token_here';
+
+const url = `${API_HOST}/finance/accounts?sort_by=name&order_by=asc&per_page=5`;
+const res = await fetch(url, {
+  headers: { 'Accept': 'application/json', 'Authorization': `Bearer ${ACCESS_TOKEN}` }
+});
+const data = await res.json();
+console.log(JSON.stringify(data, null, 2));
